beef1ebcdc0e918260939858fb4d01d8f84d3740
Fool <notascam@hotbabies.cia.govgit commitgit config user.email notascam@hotbabies.cia.gov>
2019-02-15 14:57:31 -0800

    implementation of scoremaster function

--- Assets/Manager Scripts/ScoreMaster.cs
+++ Assets/Manager Scripts/ScoreMaster.cs
@@ -17,12 +17,28 @@ public static class ScoreMaster {
 		return cumulativeScores;
 	}
 
-	public static List<int> frameScores (List <int> scores) {
-		List<int> frameList = new List<int>();
+	public static List<int> frameScores (List <int> rolls) {
+		List<int> frameList = new List<int> ();
+		int bonus = 0;
+
+		for (int i = 0; i < rolls.Count; i += 2) { //counts  by 2 (2 rolls is one frame)
+
+			if (rolls [i - 1] == 10) { //STRIKE! (bowl a perfect 10 on the first roll!
+
+			} else if (rolls [i - 1] + rolls [i] == 10) {
+
+			} else {
+				bonus = 0;
+			}
+
+			frameList.Add (rolls [i - 1] + rolls [i] + bonus);
+		}
 
 
 
 		return frameList;
+
+
 	}
 }
 
